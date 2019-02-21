dac18e430b8906b9a9f391743470d89190094d7a
Will Barber <will@rudi.org.uk>
2019-02-21 01:27:46 +0000

    Added a non-interactive mode to rofi_monitors.sh
    Fixed bugs in layout selection and restore

--- rofi_monitors.sh
+++ rofi_monitors.sh
@@ -4,7 +4,7 @@
 
 XRANDR=$(which xrandr)
 
-LAPTOP_MON='eDP1'
+LAPTOP_MON='Virtual1'
 
 if [ ! -z layouts.conf ]; then
 	touch layouts.conf
@@ -17,7 +17,7 @@ function non_interactive_display() {
 				($1 != Laptop) && /axis/ {cmd=cmd " --output " $1 " --off"} 
 				END{print cmd}' | xargs -I % sh -c "${XRANDR} %";;
 		"list" )
-			awk '{match($0,"#(.*)$",DATA); print  DATA[1]}' < layouts.conf;;
+			awk '{match($0,"#(.*)$",DATA); print  DATA[1]}' layouts.conf;;
 		"save" )
 			[ -z "$2" ]&& echo "Invalid name" && exit 0
 			${XRANDR} | awk -v NAME="$2" '
@@ -37,11 +37,9 @@ function non_interactive_display() {
 			if [[ `grep -E "#$2\$" layouts.conf | wc -l` != "1" ]]; then
 				echo "Invalid layout"
 			fi
-			echo "$2" | awk -v MONS="$( ${XRANDR} | awk '/axis/{ print $1 }' )" '
-			{
+			awk -v MONS="$( ${XRANDR} | awk '/axis/{ print $1 }' )" -v I="$2" '
+			($0 ~ "#" I "$"){
 				cmd="";
-				print $0;
-				print MONS;
 				split(MONS, MON_LIST, /[[:space:]]/);
 				for(i in MON_LIST) {
 					if($0 !~ MON_LIST[i]) {
@@ -49,7 +47,7 @@ function non_interactive_display() {
 					}
 				}
 				print cmd $0
-			}' | xargs -I % sh -c "${XRANDR} %";; # execute layout command & disable unused displays
+			}' layouts.conf | xargs -I % sh -c "${XRANDR} %";; # execute layout command & disable unused displays
 		* ) echo "Usage: $0 [reset|list|save|restore]"
 			echo "  reset           reset to laptop display only"
 			echo "  list            list saved layouts"
@@ -101,19 +99,13 @@ function contains() { # test if the string in $2 exists in the list $1
 }
 
 function gen_custom_layout_list() {
-	CUSTOM_LAYOUTS=( "$(<layouts.conf)" ) # fetch custom layouts from file
-	for i in $(seq 0 $((${#CUSTOM_LAYOUTS[@]}-1))); do # print list with index prepended
-		echo "${CUSTOM_LAYOUTS[i]}" | awk -v i="$i" '{match($0,"#(.*)$",DATA); print i " " DATA[1]}'
-	done
+	awk 'BEGIN{i=1;print "0 Cancel"} {match($0,"#(.*)$",DATA); print i++ " " DATA[1]}' layouts.conf # print list with index prepended
 }
 
 function set_custom_layout() {
-	CUSTOM_LAYOUTS=( "$(<layouts.conf)" ) # fetch custom layouts from file
-	echo "${CUSTOM_LAYOUTS[$1]}" | awk -v MONS="$( ${XRANDR} | awk '/axis/{ print $1 }' )" '
-	{
+	awk -v MONS="$( ${XRANDR} | awk '/axis/{ print $1 }' )" -v I=$1 '
+	NR==I{
 		cmd="";
-		print $0;
-		print MONS;
 		split(MONS, MON_LIST, /[[:space:]]/);
 		for(i in MON_LIST) {
 			if($0 !~ MON_LIST[i]) {
@@ -121,7 +113,7 @@ function set_custom_layout() {
 			}
 		}
 		print cmd $0
-	}' | xargs -I % sh -c "${XRANDR} %" # execute layout command & disable unused displays
+	}' layouts.conf | xargs -I % sh -c "${XRANDR} %" # execute layout command & disable unused displays
 }
 
 function create_custom_layout() {
@@ -203,8 +195,7 @@ while [[ true ]]; do
 			if [[ ! "$?" ]]||[[ -z "$MON2" ]]; then continue; fi
 			${XRANDR} --output "$MON2" --primary;;
 		5) LAYOUT=$( gen_custom_layout_list | rofi -dmenu -p "Select Layout" -a 0 -no-custom | awk '{print $1}' )
-			contains $(gen_custom_layout_list) $LAYOUT # catch invalid input, such as people pressing <esc>
-			if [[ ! "$?" ]]||[[ -z "$LAYOUT" ]]; then continue; fi
+			if [[ -z "$LAYOUT" ]]||[ ! "$LAYOUT" -ge 1 -a "$LAYOUT" -lt "$(cat layouts.conf|wc -l)" ]; then continue; fi # catch invalid input, such as people pressing <esc>
 			set_custom_layout $LAYOUT
 			exit 0;;
 		6) create_custom_layout;;
