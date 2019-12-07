eb577c438813102f8242d782b53d2c6fbfae0615
WillB97 <will@rudi.org.uk>
2019-12-07 16:53:32 +0000

    Converted rofi_i3_ws to use case statements

--- rofi_i3_ws.sh
+++ rofi_i3_ws.sh
@@ -1,17 +1,13 @@
 #! /bin/bash
 
 NAME="$(i3-msg -t get_workspaces | jq -r '.[].name,"New Workspace"'|rofi -dmenu -i -p 'Workspace')"
-if [ "$NAME" == "New Workspace" ]; then
-	NAME="$(rofi -dmenu -p "Workspace Number" -theme-str 'listview { enabled: false; }')"
-fi
-if [[ $NAME == "" ]]; then
-   return 0
-fi   
-if [ "$#" -ge 1 ]; then
-   case "$1" in
-		"move") i3-msg "move container to workspace number \"$NAME\"";;
-		"drag") i3-msg "move container to workspace number \"$NAME\"; workspace number \"$NAME\"";;
-	esac
-else
-	i3-msg "workspace number \"$NAME\""
-fi
+case "$NAME" in
+	"New Workspace")
+		NAME="$(rofi -dmenu -p "Workspace Number" -theme-str 'listview { enabled: false; }')";;
+	"") return 0;;
+esac
+case "$1" in
+	"move") i3-msg "move container to workspace number \"$NAME\"";;
+	"drag") i3-msg "move container to workspace number \"$NAME\"; workspace number \"$NAME\"";;
+	*) i3-msg "workspace number \"$NAME\"";;
+esac
